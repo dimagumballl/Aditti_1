b83bab6b83a8e0fc592b703f5cfee8089d646022
unknown <dimagumball@gmail.com>
2020-02-17 19:14:31 +0200

    Updates

---
+++ precache-manifest.e492b4380f384f53d4bbed0ea363223a.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c8da9d11374862aa0611702699df279",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b1949c49f152c49c0f00",
+    "url": "/static/css/main.b34522bd.chunk.css"
+  },
+  {
+    "revision": "aacfba9538ef8c54229a",
+    "url": "/static/js/2.67c13c49.chunk.js"
+  },
+  {
+    "revision": "d891fcc30dfc707e593ab65e09f6fef9",
+    "url": "/static/js/2.67c13c49.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1949c49f152c49c0f00",
+    "url": "/static/js/main.4c518fef.chunk.js"
+  },
+  {
+    "revision": "95bfdff9b13382152279",
+    "url": "/static/js/runtime-main.b85cbcb2.js"
+  },
+  {
+    "revision": "bde95fd4c7fcef652a3f6ee99170e09b",
+    "url": "/static/media/facebook.bde95fd4.svg"
+  },
+  {
+    "revision": "1cc0cb4236b4d9fba6a91058f1d396fa",
+    "url": "/static/media/instagram.1cc0cb42.svg"
+  },
+  {
+    "revision": "919175c7bec7fbb785706d83213e5723",
+    "url": "/static/media/mc.919175c7.svg"
+  },
+  {
+    "revision": "8d16fc56f41706d3a7a7d87bfa79242b",
+    "url": "/static/media/privat.8d16fc56.svg"
+  },
+  {
+    "revision": "e0473c0f89558403c2d73b48ef126050",
+    "url": "/static/media/search.e0473c0f.png"
+  },
+  {
+    "revision": "191977d4d85d15c543c777cbeb19b328",
+    "url": "/static/media/viber.191977d4.svg"
+  },
+  {
+    "revision": "d97b9761c885e4150f63e28387f224e9",
+    "url": "/static/media/visa.d97b9761.svg"
+  },
+  {
+    "revision": "2a1a5f5e562e404469ecead79e9eaceb",
+    "url": "/static/media/youtube.2a1a5f5e.svg"
+  }
+]);
